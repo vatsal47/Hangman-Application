932f38af6818a5d06c93a8050ee3be43bb942270
Vatsal <ramivatsal07@gmail.com>
2020-03-24 11:06:01 +0530

    converted constructor function in class

--- main.js
+++ main.js
@@ -1,59 +1,62 @@
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split(""),
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    this.status = 'playing'
-}
-
-Hangman.prototype.calculateStatus = function () {
-    const finished = this.word.every(letter => this.guessedLetters.includes(letter))
-
-    if (this.remainingGuesses === 0) {
-        this.status = 'failed'
-    } else if (finished) {
-        this.status = 'finished'
-    } else {
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split(""),
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
         this.status = 'playing'
     }
-}
 
-Hangman.prototype.getStatusMessage = function () {
-    if (this.status === 'playing') {
-        return `Guesses left: ${this.remainingGuesses}`
-    } else if (this.status === 'failed') {
-        return `Nice try! The word was '${this.word.join('')}'`
-    } else {
-        return `Great work! You guesses the work.`
-    }    
-}
+    calculateStatus() {
+        const finished = this.word.every(letter => this.guessedLetters.includes(letter))
 
-Hangman.prototype.getPuzzle = function() {
-    return this.word.map(letter => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            return letter
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
         } else {
-            return '*'
-        }
-    }).join("")
-}
-
-Hangman.prototype.makeGuess = function(guess) {
-    guess = guess.toLowerCase()
-
-    if (this.status !== 'playing') {
-        return 
-    } 
-
-    if (!this.guessedLetters.includes(guess)) {
-        this.guessedLetters.push(guess)
-
-        if (!this.word.includes(guess)) {
-            this.remainingGuesses--
+            this.status = 'playing'
         }
     }
 
-    this.calculateStatus()
+    getStatusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was '${this.word.join('')}'`
+        } else {
+            return `Great work! You guesses the work.`
+        }        
+    }
+
+    getPuzzle() {
+        return this.word.map(letter => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                return letter
+            } else {
+                return '*'
+            }
+        }).join("")
+    }
+
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+
+        if (this.status !== 'playing') {
+            return 
+        } 
+
+        if (!this.guessedLetters.includes(guess)) {
+            this.guessedLetters.push(guess)
+
+            if (!this.word.includes(guess)) {
+                this.remainingGuesses--
+            }
+        }
+
+        this.calculateStatus()
+    }
 }
+
 
 
 
